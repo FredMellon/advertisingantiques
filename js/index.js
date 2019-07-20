bbef56b229c471fd460b548b8ea6bfa972feda7e
FredMellon <fmellon83@gmail.com>
2019-07-19 22:46:19 -0400

    more changes

--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
 function breadInfo() {
-    alert('This is a vintage bread advertisement');
+    alert('This is a vintage bread advertisement.');
 }
 
 function sodaInfo() {
-    alert('This is vintage soda advertising');
+    alert('This is vintage soda advertising.');
 }
 
 function tobaccoInfo() {
-    alert('This is a vintage tobacco advertisement');
+    alert('This is a vintage tobacco advertisement.');
 }
 
-function oilInfo() {
-    alert('This is a vintage gas and oil advertisement');
+function carInfo() {
+    alert('This is a vintage car advertisement.');
 }
 
 
